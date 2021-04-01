dae37e82975064b9c283cb3161f995d7f0aff8c4
Erik Torenstam <erik.torenstam@futuregames.nu>
2021-04-01 11:22:35 +0200

    Added enemyVeriety, aliens shoot

--- FlorpyBirb/Assets/Erik/Scripts/EnemyFightBack.cs
+++ FlorpyBirb/Assets/Erik/Scripts/EnemyFightBack.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyFightBack : MonoBehaviour
 {
+    public int AlienColor;
+    public GameObject laser;
+    public Sprite[] laserSprites;
     Transform player => GameObject.Find("Player").transform;
     public EnemySpawner.Enemy enemyType;
     // if you hit the rock/enemy with the hand, you get the "GetSlapped" funtion, otherwise you just do the death function
@@ -11,18 +14,49 @@ public class EnemyFightBack : MonoBehaviour
     {
         Destroy(gameObject);
     }
+    private void Start()
+    {
+        if (enemyType == EnemySpawner.Enemy.alien) StartCoroutine(SHOOTTHECANONCAPTAIN());
+    }
+
+    IEnumerator SHOOTTHECANONCAPTAIN()
+    {
+        yield return new WaitUntil(() => player.position.x - transform.position.x < 14);
+        FireShit();
+    }
+    void FireShit()
+    {
+        GameObject lazor = Instantiate(laser, transform.position, Quaternion.Euler(0, 0, 0));
+        lazor.GetComponent<SpriteRenderer>().sprite = laserSprites[AlienColor];
+        lazor.GetComponent<Rigidbody2D>().velocity = new Vector2(-10, 0);
+        Destroy(lazor, 2);
+    }
     private void Update()
     {
-
+        
         switch (enemyType)
         {
             default:
-            case EnemySpawner.Enemy.eye:
                 {
-                    transform.rotation = Quaternion.Euler(new Vector2(0, transform.position.x - player.position.x));
+                    Debug.LogError("this path returns nothing");
                     return;
                 }
             case EnemySpawner.Enemy.tefat:
+                {
+                    return;
+                }
+            case EnemySpawner.Enemy.eye:
+                {
+                    transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+                    transform.LookAt(player);
+                    transform.rotation = Quaternion.Euler(new Vector3(0, 0, transform.rotation.eulerAngles.x));
+                    return;
+                }
+            case EnemySpawner.Enemy.alien:
+                {
+                    return;
+                }
+            case EnemySpawner.Enemy.rock:
                 {
                     return;
                 }
--- FlorpyBirb/Assets/Erik/Scripts/EnemySpawner.cs
+++ FlorpyBirb/Assets/Erik/Scripts/EnemySpawner.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    int randomAlien;
     Settings settings;
     public GameObject enenemeny;
-    public Sprite[] rockSprites, tefatSprites, eyeSprites;
-    [Range(1, 10)] public float spawnSpeed;
+    public Sprite[] rockSprites, tefatSprites, eyeSprites, alineSprites;
+    [Range(0.5f, 1)] public float spawnSpeed;
     private void Start()
     {
         settings = Settings.settings;
@@ -20,9 +21,11 @@ public class EnemySpawner : MonoBehaviour
     {
         GameObject rock;
         Enemy emily = (Enemy)Random.Range(0, (int)Enemy.maxShit);
-        rock = Instantiate(enenemeny, new Vector2(transform.position.x, Random.Range(-4.5f, 4.5f)), Quaternion.identity);
+        rock = Instantiate(enenemeny, new Vector2(transform.position.x, Random.Range(-4.5f, 4.5f)), Quaternion.Euler(0, 0, 0));
         rock.GetComponent<SpriteRenderer>().sprite = GibSprite(emily);
         rock.GetComponent<EnemyFightBack>().enemyType = emily;
+        rock.GetComponent<EnemyFightBack>().AlienColor = randomAlien;
+        if (emily == Enemy.rock || emily == Enemy.tefat) rock.GetComponent<Rigidbody2D>().angularVelocity = Random.Range(-1000, 1000);
         Destroy(rock, 5);
         Invoke("SpawnDanger", Random.Range(spawnSpeed / (int)settings.difficulty, 0.1f + spawnSpeed / (int)settings.difficulty));
     }
@@ -31,13 +34,18 @@ public class EnemySpawner : MonoBehaviour
         switch (emily)
         {
             default:
-            case Enemy.eye: return eyeSprites[Random.Range(0, eyeSprites.Length)];
-            case Enemy.tefat: return tefatSprites[Random.Range(0, tefatSprites.Length)];
-            case Enemy.rock: return rockSprites[Random.Range(0, rockSprites.Length)];
-        }
+            case Enemy.rock:    return rockSprites[Random.Range(0, rockSprites.Length)];
+            case Enemy.alien:
+                {
+                    randomAlien = Random.Range(0, alineSprites.Length);
+                    return alineSprites[randomAlien];
+                }
+            case Enemy.eye:     return eyeSprites[Random.Range(0, eyeSprites.Length)];
+        }   
     }
     public enum Enemy
     {
+        alien,
         rock,
         tefat,
         eye,
